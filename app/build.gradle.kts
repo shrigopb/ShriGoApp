22973d784d89e82dda8e8a24602a9d139fc373c2
shrifoodsGit <shrifoodspb@gmail.com>
2026-05-30 18:06:51 +0530

    Relase v6.1.5 commit- Upload rides successful

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "in.shrigo.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
