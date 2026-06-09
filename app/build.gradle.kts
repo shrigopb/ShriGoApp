404801e1757ae8108b97e73b769050b545db32c1
shrifoodsGit <shrifoodspb@gmail.com>
2026-06-09 16:58:30 +0530

    Release v12.2.2,Fixed launcher icon, branding, airport logic and share functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "in.shrigo.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "2.1"
+        versionCode = 12
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
