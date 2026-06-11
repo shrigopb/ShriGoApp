f618a786d9086fef909d2fdd17d7379ddb9d04e2
shrifoodsGit <shrifoodspb@gmail.com>
2026-06-11 16:27:31 +0530

    Release v14.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "in.shrigo.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "2.2"
+        versionCode = 14
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
