abee8acc0d58a35df1173fb3841598f3c0055e00
shrifoodsGit <shrifoodspb@gmail.com>
2026-06-15 15:46:45 +0530

    ShriGo R v15.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "in.shrigo.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "2.4"
+        versionCode = 15
+        versionName = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
