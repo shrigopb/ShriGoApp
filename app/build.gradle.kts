bc5f2db4f40d91ab3b7e8c6472f1179271c30006
shrifoodsGit <shrifoodspb@gmail.com>
2026-06-27 18:07:22 +0530

    SG R_v19.2.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "in.shrigo.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 18
-        versionName = "2.8"
+        versionCode = 19
+        versionName = "2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
